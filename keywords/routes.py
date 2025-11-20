9ef776a9617a558603c4ab843d382779bdd0eb1f
Kurisu Naotsugu <kurikurikurusu@gmail.com>
2025-11-21 06:56:20 +0900

    Implement weekly trend graph for PubMed article counts with user input for keywords and weeks

--- keywords/routes.py
+++ keywords/routes.py
@@ -1,6 +1,21 @@
-from flask import Blueprint, render_template
-from . import keywords_bp  # initで定義されたBlueprintインスタンスをインポート
+from flask import Blueprint, render_template, request
+from .modules import fetch_weekly_counts
+from . import keywords_bp  
 
 @keywords_bp.route('/keywords')
-def product_home():
-    return "keywirds Page"
+def weekly_graph():
+    labels = []
+    values = []
+
+    if request.method == 'POST':
+        keywords_str = request.form.get('keywords', '')
+        weeks = int(request.form.get('weeks', 8))  # デフォルト8週間
+
+        keywords = [k.strip() for k in keywords_str.split() if k]
+
+        if keywords:
+            data = fetch_weekly_counts(keywords, weeks)
+            labels = list(data.keys())
+            values = list(data.values())
+
+    return render_template('trend.html', labels=labels, values=values)
